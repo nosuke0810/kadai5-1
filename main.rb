bb0bb3ea5ca2f0fc4937473de505f1bf00d26515
e1342 <e1342@PC4-22.cc.matsue-ct.jp>
2017-09-26 17:08:36 +0900

    追加しました

--- main.rb
+++ main.rb
@@ -3,32 +3,42 @@ require 'dxruby'
 
 require_relative 'player'
 require_relative 'enemy'
+require_relative 'painn'
 
 Window.width  = 800
 Window.height = 600
 
-player_img = Image.load("player.png")
+player_img = Image.load("apple.png")
 player_img.setColorKey([0, 0, 0])
 
-enemy_img = Image.load("enemy.png")
+enemy_img = Image.load("penn.png")
 enemy_img.setColorKey([0, 0, 0])
 
-player = Player.new(400, 500, player_img)
+pain_img = Image.load("pain.png")
+pain_img.setColorKey([0, 0, 0])
 
+
+
+players = [Player2.new(600, 500, player_img),Player1.new(200, 500, pain_img)]
+
+  
 enemies = []
-10.times do
-  enemies << Enemy.new(rand(800), rand(600), enemy_img)
+30.times do
+  
 end
 
 Window.loop do
+  enemies << Enemy.new(rand(800), rand(600), enemy_img)
   break if Input.keyPush?(K_ESCAPE)
 
   Sprite.update(enemies)
   Sprite.draw(enemies)
 
-  player.update
-  player.draw
+  Sprite.update(players)
+  Sprite.draw(players)
+
+
 
   # 当たり判定
-  Sprite.check(player, enemies)
+  Sprite.check(players, enemies)
 end
--- player.rb
+++ player.rb
@@ -1,8 +1,18 @@
 # coding: utf-8
 
-class Player < Sprite
+
+class Player1<Sprite
   def update
-    self.x += Input.x
-    self.y += Input.y
+
+    self.x += Input.x*3
+    self.y += Input.y*3
+  end
+end
+
+class Player2<Sprite
+  def update
+
+    self.x = Input.mouse_pos_x  # マウスカーソルのx座標
+    self.y = Input.mouse_pos_y  # マウスカーソルのy座標
   end
 end
